b29ebe04ba9bd91ddf457c2189907a242606a70c
zamietka <remaczek@proton.me>
2025-10-30 22:03:21 +0100

    30102025

--- neocities-2044/music.js
+++ neocities-2044/music.js
@@ -9,22 +9,51 @@ const musicTitle = document.querySelector('.songname')
 const musicCat = document.querySelector('#musiccat')
 let isPlaying = 0
 
-//Song titles
-//const songList = ['lala','Savestate - Evil Dr. Cool & His Radical Sunglasses','Gyms - Dog Town']
-
 const songList = [
   {
-    title: 'lala',
-    src: 'https://example.com/audio/lala.mp3'
+    title: 'Commodore 64 Lemmings Music - Lemming 1 (Pachelbel\'s Canon)',
+    src: 'https://github.com/zamietka/2044-website/raw/refs/heads/main/neocities-2044/Commodore%2064%20Lemmings%20Music%20-%20Lemming%201%20(Pachelbel\'s%20Canon).mp3'
+  },
+  {
+    title: 'A Beautiful Song - 8 bit',
+    src: 'https://github.com/zamietka/2044-website/raw/refs/heads/main/neocities-2044/A%20Beautiful%20Song%20-%208%20bit.mp3'
   },
   {
     title: 'Savestate - Evil Dr. Cool & His Radical Sunglasses',
-    src: 'https://docs.google.com/uc?export=download&id=1l8U5Zo655AHmHh2iaIo6S6oTT1Z2kTWV'
+    src: 'https://github.com/zamietka/2044-website/raw/refs/heads/main/neocities-2044/Savestate%20-%20Evil%20Dr.%20Cool%20&%20His%20Radical%20Sunglasses.mp3'
   },
   {
-    title: 'Gyms - Dog Town',
-    src: 'https://example.com/audio/gyms.mp3'
-  }
+    title: 'Deltarune - The Legend',
+    src: 'https://github.com/zamietka/2044-website/raw/refs/heads/main/neocities-2044/Deltarune%20-%20The%20Legend.mp3'
+  },
+  {
+    title: 'DuckTales Music (NES) - The Himalayas Theme',
+    src: 'https://github.com/zamietka/2044-website/raw/refs/heads/main/neocities-2044/DuckTales%20Music%20(NES)%20-%20The%20Himalayas%20Theme.mp3'
+  },
+  {
+    title: 'EarthBound Beginnings - Being friends',
+    src: 'https://github.com/zamietka/2044-website/raw/refs/heads/main/neocities-2044/EarthBound%20Beginnings%20-%20Being%20friends.mp3'
+  },
+  {
+    title: 'FAITH Soundtrack l Beethoven.mp3',
+    src: 'https://github.com/zamietka/2044-website/raw/refs/heads/main/neocities-2044/FAITH%20Soundtrack%20l%20Beethoven.mp3'
+  },
+  {
+    title: 'In The End [8 Bit Tribute to Chester Bennington (RIP) & Linkin Park] - 8 Bit Universe.mp3',
+    src: 'https://github.com/zamietka/2044-website/raw/refs/heads/main/neocities-2044/In%20The%20End%20%5B8%20Bit%20Tribute%20to%20Chester%20Bennington%20(RIP)%20&%20Linkin%20Park%5D%20-%208%20Bit%20Universe.mp3'
+  },
+  {
+    title: 'Legend of Zelda, The (NES) Music - Ending Theme.mp3',
+    src: 'https://github.com/zamietka/2044-website/raw/refs/heads/main/neocities-2044/Legend%20of%20Zelda,%20The%20(NES)%20Music%20-%20Ending%20Theme.mp3'
+  },
+  {
+    title: 'Pokémon Gold & Silver - Routes 26-27.mp3',
+    src: 'https://github.com/zamietka/2044-website/raw/refs/heads/main/neocities-2044/Pok%C3%A9mon%20Gold%20&%20Silver%20-%20Routes%2026-27.mp3'
+  },
+  {
+    title: 'Zombie [8 Bit Tribute to Bad Wolves and the Cranberries] - 8 Bit Universe.mp3',
+    src: 'https://github.com/zamietka/2044-website/raw/refs/heads/main/neocities-2044/Zombie%20%5B8%20Bit%20Tribute%20to%20Bad%20Wolves%20and%20the%20Cranberries%5D%20-%208%20Bit%20Universe.mp3'
+  },
 ];
 
 function getRandomInt(max) {
