46c5392886301295058cdda837a00de0091ca66f
zamietka <remaczek@proton.me>
2025-11-12 23:14:14 +0100

    draggable to bottom of selected divs

--- neocities-2044/friend.js
+++ neocities-2044/friend.js
@@ -1,15 +1,34 @@
 const friendcontainer = document.querySelector('#friendcontainer');
+const main = document.querySelector('#main')
 const page = document.querySelector('#page');
+const page2 = document.querySelector('#page2');
 let isMouseDown = false;
+pageHover = 0;
 
+
+
+page.addEventListener('mouseover', () => {
+    pageHover = 1
+});
+page2.addEventListener('mouseover', () => {
+    pageHover = 2
+});
+
+page.addEventListener('mouseout', () => {
+    pageHover = 0
+});
+page2.addEventListener('mouseout', () => {
+    pageHover = 0
+});
 
 friendcontainer.addEventListener('mousedown', (e) => {
     isMouseDown = true;
+    friendcontainer.style.pointerEvents = 'none';
     x = e.clientX - friendcontainer.offsetLeft;
     y = e.clientY - friendcontainer.offsetTop;
 });
 
-page.addEventListener('mousemove', (e) => {
+main.addEventListener('mousemove', (e) => {
     fall = e.clientY - y
     if (isMouseDown) {
         friendcontainer.style.left = `${e.clientX - x}px`;
@@ -17,13 +36,25 @@ page.addEventListener('mousemove', (e) => {
     }
 });
 
-page.addEventListener('mouseup', () => {
+main.addEventListener('mouseup', () => {
     isMouseDown = false;
-    friendcontainer.style.top = `${page.offsetHeight - friendcontainer.offsetHeight}px`;
+    friendcontainer.style.pointerEvents = 'auto';
+    if (pageHover === 2){
+    friendcontainer.style.top = `${page2.offsetTop - friendcontainer.offsetHeight + page2.offsetHeight}px`;
+    }
+    else if (pageHover === 1) {
+        friendcontainer.style.top = `${page.offsetTop - friendcontainer.offsetHeight + page.offsetHeight}px`;
+    }
+    else if (pageHover === 0) {
+        friendcontainer.style.top = `${main.offsetHeight - friendcontainer.offsetHeight}px`;
+    }
     friendcontainer.style.transition = 'top 0.5s ease';
     setTimeout(() => {
     friendcontainer.style.transition = '';
   }, 500);
 
 });
+
+
     
+
