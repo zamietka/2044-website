fbbd8791065c67f2fd306f1d694b76c693ab975b
zamietka <remaczek@proton.me>
2025-11-16 20:22:07 +0100

    working walking

--- neocities-2044/friend.js
+++ neocities-2044/friend.js
@@ -1,60 +1,128 @@
 const friendcontainer = document.querySelector('#friendcontainer');
-const main = document.querySelector('#main')
-const page = document.querySelector('#page');
-const page2 = document.querySelector('#page2');
+const flex = document.querySelector('#flex')
+const ghostdiv1 = document.querySelector('#ghostdiv1');
+const ghostdiv2 = document.querySelector('#ghostdiv2');
 let isMouseDown = false;
-pageHover = 0;
+ghostHover = 0;
+catAwake = 0;
+
+randomActions () 
 
 
+ghostdiv1.addEventListener('mouseover', () => {
+    ghostHover = 1
+});
+ghostdiv2.addEventListener('mouseover', () => {
+    ghostHover = 2
+});
 
-page.addEventListener('mouseover', () => {
-    pageHover = 1
+ghostdiv1.addEventListener('mouseout', () => {
+    ghostHover = 0
 });
-page2.addEventListener('mouseover', () => {
-    pageHover = 2
+ghostdiv2.addEventListener('mouseout', () => {
+    ghostHover = 0
 });
 
-page.addEventListener('mouseout', () => {
-    pageHover = 0
-});
-page2.addEventListener('mouseout', () => {
-    pageHover = 0
-});
+friendcontainer.addEventListener('click', () => {
+    friendcontainer.src = "images/animacjawake2.gif";
+    catAwake = 1;
+    setTimeout(() => {
+    friendcontainer.src ="images/animacjaidle.gif"
+  }, 1200);
+    
+})
+
 
 friendcontainer.addEventListener('mousedown', (e) => {
+    friendcontainer.style.transition = '';
+    if (catAwake === 1){ 
+    friendcontainer.src ="images/animacjadangle.gif"
     isMouseDown = true;
     friendcontainer.style.pointerEvents = 'none';
+    friendcontainer.style.left = `${e.clientX - 30}px`;
+    friendcontainer.style.top = `${e.clientY + 10}px`;
     x = e.clientX - friendcontainer.offsetLeft;
     y = e.clientY - friendcontainer.offsetTop;
-});
-
-main.addEventListener('mousemove', (e) => {
-    fall = e.clientY - y
-    if (isMouseDown) {
-        friendcontainer.style.left = `${e.clientX - x}px`;
-        friendcontainer.style.top = `${fall}px`;
     }
 });
 
-main.addEventListener('mouseup', () => {
+flex.addEventListener('mousemove', (e) => {
+    
+    if (isMouseDown) {
+        friendcontainer.style.left = `${e.clientX - 30}px`;
+        friendcontainer.style.top = `${e.clientY + 10}px`;
+    }
+});
+
+flex.addEventListener('mouseup', () => {
     isMouseDown = false;
     friendcontainer.style.pointerEvents = 'auto';
-    if (pageHover === 2){
-    friendcontainer.style.top = `${page2.offsetTop - friendcontainer.offsetHeight + page2.offsetHeight}px`;
+    if (ghostHover === 2){
+    friendcontainer.style.top = `${ghostdiv2.offsetTop - friendcontainer.offsetHeight + ghostdiv2.offsetHeight}px`;
     }
-    else if (pageHover === 1) {
-        friendcontainer.style.top = `${page.offsetTop - friendcontainer.offsetHeight + page.offsetHeight}px`;
+    else if (ghostHover === 1) {
+        friendcontainer.style.top = `${ghostdiv1.offsetTop - friendcontainer.offsetHeight + ghostdiv1.offsetHeight}px`;
     }
-    else if (pageHover === 0) {
-        friendcontainer.style.top = `${main.offsetHeight - friendcontainer.offsetHeight}px`;
+    else if (ghostHover === 0 && catAwake === 1) {
+        friendcontainer.style.top = `${flex.offsetHeight - friendcontainer.offsetHeight}px`;
     }
     friendcontainer.style.transition = 'top 0.5s ease';
     setTimeout(() => {
     friendcontainer.style.transition = '';
-  }, 500);
+    }, 500);
+    friendcontainer.src ="images/animacjaidle.gif"
 
 });
+function randomActions () {
+    friendcontainer.style.transition = '';
+    const random = Math.floor(Math.random() * 3);
+    const randomstep = Math.floor(Math.random() * 7 + 1);
+    const delay = Math.floor(Math.random() * 3000) + 2000;
 
+    setTimeout(() => {
 
-    
+        if (isMouseDown) {
+            return randomActions(); 
+        }
+
+        if (catAwake !== 1) {
+            return randomActions();
+        }
+
+        if (random === 1) {
+            friendcontainer.src ="images/animacja-right.gif";
+            for (let step = 0; step < randomstep; step++) {
+                setTimeout(() => {
+                    if (isMouseDown === false) {
+                        friendcontainer.style.left = `${friendcontainer.offsetLeft + 50}px`;
+                        friendcontainer.style.transition = 'left 0.5s ease';
+                    }
+                }, step * 500);
+            }
+            
+        }
+
+        if (random === 2) {
+            friendcontainer.src ="images/animacja-LEFT.gif";
+            for (let step = 0; step < randomstep; step++) {
+                setTimeout(() => {
+                    if (isMouseDown === false) {
+                        friendcontainer.style.left = `${friendcontainer.offsetLeft - 50}px`;
+                        friendcontainer.style.transition = 'left 0.5s ease';
+                    }
+                }, step * 500);
+            }
+            
+        }
+
+        setTimeout(() => {
+            if (isMouseDown === false) {
+                friendcontainer.src ="images/animacjaidle.gif";
+            }
+            randomActions();
+        }, randomstep * 500);
+
+    }, delay);
+}
+
 
